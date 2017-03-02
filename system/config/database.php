575cde5608f88ceddb50dba534d705a1f569f8c5
isLishude <lishdue@foxmail.com>
2017-03-02 16:27:09 +0800

    init

---
+++ system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => 'lishude',
+  'dbname' => 'wc',
+);
+$config['slave'] = false;
---
+++ tmp/plugins_model.php
@@ -0,0 +1,1 @@
+a:2:{s:18:"aws_external_class";s:54:"E:\wamp64\www\wc/plugins/aws_external/aws_external.php";s:26:"aws_offical_external_class";s:70:"E:\wamp64\www\wc/plugins/aws_offical_external/aws_offical_external.php";}
